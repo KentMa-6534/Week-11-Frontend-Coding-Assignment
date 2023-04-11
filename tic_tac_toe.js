a7eed5e2e43e857d22d5556cf54d9b1fd6ea7382
KentMa-6534 <realkentma@gmail.com>
2023-04-11 09:55:26 -0700

    Week-11-Frontent-Coding-Assignment

---
+++ tic_tac_toe.js
@@ -0,0 +1,70 @@
+let playerText = $('#playerText');
+let restartBtn = $('#restartBtn');
+let boxes = Array.from($('.box'));
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks');
+const strike = $('#strike');
+
+const O_SYMBOL = "O";
+const X_SYMBOL = "X";
+
+let currentPlayer = X_SYMBOL;
+let spaces = Array(9).fill(null);
+
+const startGame = () => {
+    boxes.forEach (box => box.addEventListener('click', boxClicked));
+}
+
+function boxClicked(e) {
+    const id = e.target.id;
+    $('audio#pop')[0].play();
+    if(!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        if(playerHasWon() !== false){
+            playerText = `${currentPlayer} has won!`;
+            let winning_blocks = playerHasWon();
+
+            winning_blocks.map(box => boxes[box].style.backgroundColor = winnerIndicator);
+            $('audio#applause')[0].play();
+            return;
+        }
+
+        currentPlayer = currentPlayer == X_SYMBOL ? O_SYMBOL: X_SYMBOL;
+    }
+}
+
+const winningCombos = [
+    [0,1,2], [3,4,5],
+    [6,7,8], [0,3,6],
+    [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6],
+   
+]
+
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let  [a,b,c] = condition;
+
+        if(spaces[a] && (spaces[a] == spaces[b]) && spaces[a] == spaces [c]){
+            return [a,b,c];
+        }
+    }
+    return false;
+}
+
+restartBtn.click(restart);
+
+function restart() {
+    spaces.fill(null);
+
+    boxes.forEach(box => {
+        box.innerText = '';
+        currentPlayer = X_SYMBOL;
+        box.style.backgroundColor = '';
+    })
+
+    playerText = 'Tic Tac Toe';
+}
+
+startGame();
